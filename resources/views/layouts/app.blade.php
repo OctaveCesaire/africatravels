5b11b5bab4cdca73f3c66b4d75aee65cc6c2e680
Octave Cesaire <ahmedoctavemama@outlook.fr>
2024-12-23 18:09:27 +0100

    DOssier à remplacer par restart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,38 @@
+
+
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon.png">
+        <link rel="icon" type="image/png" href="{{ asset('assets/img/elite-technologie.png') }}">
+        <title>
+            {{ env('APP_NAME') }}
+        </title>
 
-        <title>{{ config('app.name', 'AfricaTravels') }}</title>
+        <!--     Fonts and icons     -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
         
         <!-- Nucleo Icons -->
         <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
         <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
 
+        <!-- Font Awesome Icons -->
+        <link href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+
         <!-- CSS Files -->
         <link href="{{ asset('assets/css/argon-dashboard.css') }}" rel="stylesheet" />
         <link href="{{ asset('assets/css/corporate-ui-dashboard.css') }}" rel="stylesheet" />
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @if (auth()->user()->hasRole('admin'))
                 @include('layouts.arc.navigation')
             @else
@@ -31,41 +40,49 @@
             @endif
 
             <!-- Page Heading -->
-            @isset($header)
+            @if(isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endisset
+            @endif
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
-        
+
         <!-- Scripts -->
         <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
         <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
         <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
         <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+        <script>
+            var win = navigator.platform.indexOf('Win') > -1;
+            if (win && document.querySelector('#sidenav-scrollbar')) {
+                var options = {
+                    damping: '0.5'
+                }
+                Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+            }
+        </script>
+
+        <!-- SweetAlert and jQuery -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
         <!-- Dashboard Scripts -->
         <script src="{{ asset('assets/js/argon-dashboard.js') }}"></script>
         <script src="{{ asset('assets/js/corporate-ui-dashboard.min.js') }}"></script>
 
-        {{-- SWEETALERT & JQUERY --}}
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-
-        @stack('js');
         <!-- Stack Pour Le Graphisme ( Global, Graphe ) -->
-        {{-- <script src="https://soft-ui-dashboard-pro-laravel.creative-tim.com/assets/js/plugins/chartjs.min.js"></script>
+        <script src="https://soft-ui-dashboard-pro-laravel.creative-tim.com/assets/js/plugins/chartjs.min.js"></script>
         <script src="https://soft-ui-dashboard-pro-laravel.creative-tim.com/assets/js/plugins/threejs.js"></script>
-        <script src="https://soft-ui-dashboard-pro-laravel.creative-tim.com/assets/js/plugins/orbit-controls.js"></script> --}}
+        <script src="https://soft-ui-dashboard-pro-laravel.creative-tim.com/assets/js/plugins/orbit-controls.js"></script>
 
-        
+        <!-- Stack JS scripts -->
+        @stack('js')
     </body>
 </html>
